5c20e0044d5e018ae9c927df8292f3bb4ca0e33b
amitkuzi <amitkuzi@gmail.com>
2024-04-30 18:27:12 +0300

    login view

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,15 +2,19 @@ import { Component } from '@angular/core';
 import { AuthServiceService } from '../../Services/auth-service.service';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
-
+import { MatDividerModule } from '@angular/material/divider';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatInputModule, MatIconModule],
+  imports: [MatInputModule, MatIconModule, MatDividerModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  LogIn() {
+    throw new Error('Method not implemented.');
+  }
+  hide: boolean = true;
   constructor(private authService: AuthServiceService) {
 
     console.log('LoginComponent constructor called IsAuthenticated: ', authService.IsAuthenticated);
