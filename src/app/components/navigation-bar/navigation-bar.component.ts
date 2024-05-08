8f1986946fb41b55907fa2294bde9bb488120bdf
amitkuzi <amitkuzi@gmail.com>
2024-05-08 21:33:39 +0300

    loading

--- src/app/components/navigation-bar/navigation-bar.component.ts
+++ src/app/components/navigation-bar/navigation-bar.component.ts
@@ -2,15 +2,18 @@ import { Component } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
-
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-navigation-bar',
   standalone: true,
-  imports: [MatSidenavModule, MatListModule, MatIconModule],
+  imports: [MatSidenavModule, MatListModule, MatIconModule, MatButtonModule],
   templateUrl: './navigation-bar.component.html',
   styleUrl: './navigation-bar.component.css'
 })
 export class NavigationBarComponent {
+  hideDropDown() {
+    throw new Error('Method not implemented.');
+  }
 
 }
