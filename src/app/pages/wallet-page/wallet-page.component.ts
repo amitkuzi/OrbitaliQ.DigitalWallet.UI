ece90c28efcbf595594cd2d2bddf7a0532799964
amitkuzi <amitkuzi@gmail.com>
2024-05-20 11:59:13 +0300

    OTF tag

--- src/app/pages/wallet-page/wallet-page.component.ts
+++ src/app/pages/wallet-page/wallet-page.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationBarComponent } from "../../components/navigation-bar/navigation-bar.component";
 import { CommonModule, NgIf } from '@angular/common';
+import { DashboardService } from '../../Services/server-api';
+import { GlobalGetUserId, InitServiceConfig } from '../../app.component';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
 
 @Component({
     selector: 'app-wallet-page',
@@ -13,18 +16,23 @@ import { CommonModule, NgIf } from '@angular/common';
     ]
 })
 export class WalletPageComponent implements OnInit {
+  OTFTag$: BehaviorSubject<string> = new BehaviorSubject<string>('');
   StartTopup() {
 
     this.topupState = !this.topupState;
       console.log('StartTopup' , this.topupState );
 }
-
  topupState: boolean = false;
+ constructor(private dashboard: DashboardService) {
+    InitServiceConfig(dashboard.configuration);
+  }
   
- 
-  constructor() {}
-  
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.dashboard.applicationDashboardOTFTagUserIdGet(GlobalGetUserId()).subscribe((data) => {
+      console.log('WalletPageComponent data: ', data);
+      this.OTFTag$.next(data);
+    });
+  }
 
 }
  
