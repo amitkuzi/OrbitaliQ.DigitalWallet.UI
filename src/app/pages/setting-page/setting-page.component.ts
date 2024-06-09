d1bdbe4d33dbec8a06d0e04892b1b176f68fbd60
amitkuzi <amitkuzi@gmail.com>
2024-06-09 13:50:55 +0300

    delete user : NOT tested

--- src/app/pages/setting-page/setting-page.component.ts
+++ src/app/pages/setting-page/setting-page.component.ts
@@ -14,6 +14,8 @@ import { MatNativeDateModule } from '@angular/material/core';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import { MatExpansionModule, MatExpansionPanel } from '@angular/material/expansion';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { AuthServiceService } from '../../Services/auth-service.service';
 
 @Component({
     selector: 'app-setting-page',
@@ -22,7 +24,7 @@ import { CommonModule } from '@angular/common';
     styleUrl: './setting-page.component.css',
     imports: [
         CommonModule,
-            MatCardModule,
+        MatCardModule,
         MatFormFieldModule,
         MatInputModule,
         MatButtonModule,
@@ -37,12 +39,14 @@ import { CommonModule } from '@angular/common';
         NavigationBarComponent]
 })
 export class SettingPageComponent implements OnInit {
+
     waiting: boolean = false;
     userDetail$: BehaviorSubject<FullUserDto> = new BehaviorSubject<FullUserDto>({});
     user: FullUserDto = {};
     startAt: Date;
     userDetailForm: FormGroup;
-    constructor(private setting: SettingService,private formBuilder: FormBuilder) {
+     
+    constructor(private setting: SettingService, private formBuilder: FormBuilder, private authService: AuthServiceService, private router: Router) {
         InitServiceConfig(setting.configuration);
         this.userDetailForm = this.formBuilder.group({
             firstNameCtrl: [''],
@@ -59,27 +63,47 @@ export class SettingPageComponent implements OnInit {
         });
 
         this.startAt = new Date(1990, 0, 1);
- }
+    }
     ngOnInit(): void {
         this.setting.applicationSettingUserDetailsUserIdGet(GlobalGetUserId()).subscribe((data) => {
             console.log('SettingPageComponent data: ', data);
             if (data.birthDate) {
                 data.birthDate = data.birthDate.split('T')[0];
-                if(data.birthDate === '0001-01-01') {
+                if (data.birthDate === '0001-01-01') {
                     data.birthDate = null;
                 }
             }
-            this.userDetail$.next(data) ;
+            this.userDetail$.next(data);
         });
     }
 
     updateUser() {
         this.waiting = true;
         console.log('updateUser', this.user);
-        this.setting.applicationSettingUserDetailsPut(this.user ).subscribe((data) => {
+        this.setting.applicationSettingUserDetailsPut(this.user).subscribe((data) => {
             console.log('updateUser data: ', data);
             this.userDetail$.next(data);
             this.waiting = false;
         });
-     }
+    }
+    deleteUser() {
+        var currentUserId = GlobalGetUserId();
+        this.setting.applicationSettingUserDetailsUserIdDelete(currentUserId).subscribe((data) => {
+            console.log('deleteUser data: ', data);
+            if (currentUserId === data) {
+                this.authService.Logout().then((result) => {
+                    console.log('Logout result: ', result);
+                    this.router.navigate(['/']);
+                });
+            }
+            else {
+                console.warn('deleteUser failed: ', data);
+            }
+        }, (error) => {console.warn('deleteUser failed: ', error);});
+
+        this.authService.Logout().then((result) => {
+            console.log('Logout result: ', result);
+            this.router.navigate(['/']);
+        });
+    }
 }
